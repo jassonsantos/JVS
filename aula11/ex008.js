81ee4d66321485a3f17559130aaa9b95348f9242
Jasson <119083963+jassonsantos@users.noreply.github.com>
2023-04-13 18:03:31 -0300

    aprendendo condiçoes e manupulaçao de condicoes

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel= 61
+console.log(`A Velocidade do seu carro é ${vel}Km/h `)
+if(vel > 60){
+    console.log('Voce ultrapassou a velocidade permitida.  Multado!')
+}
+console.log('Dirija sempre usando cinto de segurança')
+
